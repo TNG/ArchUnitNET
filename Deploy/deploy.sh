020bd8e84b5d841da80766ccb59527838ed22848
Paula Ruiz <paularuiz22@gmail.com>
2019-07-25 16:06:08 +0200

    update deployment instructions to include pack and publish as NuGet Package

--- Deploy/deploy.sh
+++ Deploy/deploy.sh
@@ -1,6 +1,5 @@
 ApiKey=$1
 Source=$2
 
-nuget pack ./ArchUnitNET/ArchUnitNET.nuspec -Verbosity detailed
 
-nuget push ./DnDGen.ArchUnitNET.*.nupkg -Verbosity detailed -ApiKey $ApiKey -Source $Source
+nuget push ./ArchUnitNET.*.nupkg -Verbosity detailed -ApiKey $ApiKey -Source $Source
