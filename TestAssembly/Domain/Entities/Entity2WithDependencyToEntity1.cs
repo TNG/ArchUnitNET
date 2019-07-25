28e04a97152b4b070046a62454b82ce37d9670b8
Paula Ruiz <paularuiz22@gmail.com>
2019-07-25 16:47:48 +0200

    change author email
    Paula Ruiz will not be with TNG much longer, so her email was updated
    to her private email address.

--- TestAssembly/Domain/Entities/Entity2WithDependencyToEntity1.cs
+++ TestAssembly/Domain/Entities/Entity2WithDependencyToEntity1.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * Copyright 2019 Florian Gather <florian.gather@tngtech.com>
- * Copyright 2019 Paula Ruiz <paula.ruiz@tngtech.com>
+ * Copyright 2019 Paula Ruiz <paularuiz22@gmail.com>
  *
  * SPDX-License-Identifier: Apache-2.0
  */
