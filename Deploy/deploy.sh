b11edf84fdd359d11772ba55ebcaab90f6212cc6
Paula Ruiz <paularuiz22@gmail.com>
2019-07-26 10:03:47 +0200

    update deploy script to add newly created nuget packages to git

--- Deploy/deploy.sh
+++ Deploy/deploy.sh
@@ -4,3 +4,6 @@ Source=$2
 
 nuget pack ArchUnitNET.csproj
 nuget push ./ArchUnitNET.*.nupkg -Verbosity detailed -ApiKey $ApiKey -Source $Source
+git add ./ArchUnitNet/nupkgs/*
+git add ./TestAssembly/nupkgs/*
+git commit -m "add NuGet packages from current build"
