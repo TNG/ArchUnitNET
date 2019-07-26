3c799af30cacd0d0408bf0429e641ee07f003ae6
Paula Ruiz <paularuiz22@gmail.com>
2019-07-26 10:14:17 +0200

    remove git add from script

--- Deploy/deploy.sh
+++ Deploy/deploy.sh
@@ -4,5 +4,3 @@ Source=$2
 
 nuget pack ArchUnitNET.csproj
 nuget push ./ArchUnitNET.*.nupkg -Verbosity detailed -ApiKey $ApiKey -Source $Source
-git add ./ArchUnitNet/nupkgs/*
-git add ./TestAssembly/nupkgs/*
