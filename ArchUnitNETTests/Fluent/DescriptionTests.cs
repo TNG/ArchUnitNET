19a23735ddb536e26c3c8db88cf512f3d81f3a2c
Fritz Brandhuber <fritz.brandhuber@tngtech.com>
2019-09-20 14:22:07 +0200

    Added TypeSyntaxElements Tests. Renamed some Test Classes slightly
    Signed-off-by: Fritz Brandhuber <fritz.brandhuber@tngtech.com>

--- ArchUnitNETTests/Fluent/DescriptionTests.cs
+++ ArchUnitNETTests/Fluent/DescriptionTests.cs
@@ -6,9 +6,9 @@ namespace ArchUnitNETTests.Fluent
 {
     public class DescriptionTests
     {
-        private readonly IArchRule _descriptionTestRule = Classes().That().HaveNameStartingWith("test").And().AreNotNested()
-            .Should().BeAbstract().OrShould().NotBeAbstract().And(Types().Should().BeNested()).Or().Attributes()
-            .Should().Exist();
+        private readonly IArchRule _descriptionTestRule = Classes().That().HaveNameStartingWith("test").And()
+            .AreNotNested().Should().BeAbstract().OrShould().NotBeAbstract().And(Types().Should().BeNested()).Or()
+            .Attributes().Should().Exist();
 
         private const string ExpectedDescription =
             "Classes that have name starting with \"test\" and are not nested should be abstract or should not be abstract and Types should be nested or Attributes should exist";
