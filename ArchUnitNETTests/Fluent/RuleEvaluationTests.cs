7032ace9eec085e34996a3bf2c4bc49104d5dfc2
Fritz Brandhuber <fritz.brandhuber@tngtech.com>
2019-09-23 19:41:56 +0200

    Moved ErrorMessage Test in RuleEvaluationTests. Added Tests for Assert.CheckArchRule()
    Signed-off-by: Fritz Brandhuber <fritz.brandhuber@tngtech.com>

--- ArchUnitNETTests/Fluent/RuleEvaluationTests.cs
+++ ArchUnitNETTests/Fluent/RuleEvaluationTests.cs
@@ -2,13 +2,15 @@
 using ArchUnitNET.Fluent;
 using ArchUnitNET.Fluent.Extensions;
 using ArchUnitNETTests.Domain;
+using ArchUnitNETTests.Fluent.Extensions;
 using Xunit;
+using Xunit.Sdk;
 using static System.Environment;
 using static ArchUnitNET.Fluent.ArchRuleDefinition;
 
-namespace ArchUnitNETTests.Fluent.Extensions
+namespace ArchUnitNETTests.Fluent
 {
-    public class EnumerableExtensionsTests
+    public class RuleEvaluationTests
     {
         private static readonly Architecture Architecture = StaticTestArchitectures.ArchUnitNETTestArchitecture;
         private const string NoClassName = "NotTheNameOfAnyClass_1592479214";
@@ -101,12 +103,50 @@ namespace ArchUnitNETTests.Fluent.Extensions
             NewLine + "\tArchUnitNETTests.Domain.PublicTestClass does exist and is not private" +
             NewLine + NewLine;
 
+        [Fact]
+        public void AssertCheckArchRuleTests()
+        {
+            Assert.CheckArchRule(Architecture, TrueArchRule1);
+            Assert.CheckArchRule(Architecture, TrueArchRule2);
+            var exception1 =
+                Assert.Throws<FailedArchRuleException>(() => Assert.CheckArchRule(Architecture, WrongArchRule1));
+            var exception2 =
+                Assert.Throws<FailedArchRuleException>(() => Assert.CheckArchRule(Architecture, WrongArchRule2));
+            var exception3 =
+                Assert.Throws<FailedArchRuleException>(() => Assert.CheckArchRule(Architecture, WrongArchRule3));
+            var exception4 =
+                Assert.Throws<FailedArchRuleException>(() => Assert.CheckArchRule(Architecture, WrongArchRule4));
+            var exception5 =
+                Assert.Throws<FailedArchRuleException>(() => Assert.CheckArchRule(Architecture, WrongArchRule5));
+            var exception6 =
+                Assert.Throws<FailedArchRuleException>(() => Assert.CheckArchRule(Architecture, WrongArchRule6));
+            var exception7 =
+                Assert.Throws<FailedArchRuleException>(() => Assert.CheckArchRule(Architecture, WrongArchRule7));
+            var exception8 =
+                Assert.Throws<FailedArchRuleException>(() => Assert.CheckArchRule(Architecture, WrongArchRule8));
+            var exception1And3 = Assert.Throws<FailedArchRuleException>(() =>
+                Assert.CheckArchRule(Architecture, WrongArchRule1AndWrongArchRule3));
+            var exception4And8 = Assert.Throws<FailedArchRuleException>(() =>
+                Assert.CheckArchRule(Architecture, WrongArchRule4AndWrongArchRule8));
+
+            Assert.Equal(_expectedWrongArchRule1ErrorMessage, exception1.Message);
+            Assert.Equal(_expectedWrongArchRule2ErrorMessage, exception2.Message);
+            Assert.Equal(_expectedWrongArchRule3ErrorMessage, exception3.Message);
+            Assert.Equal(_expectedWrongArchRule4ErrorMessage, exception4.Message);
+            Assert.Equal(_expectedWrongArchRule5ErrorMessage, exception5.Message);
+            Assert.Equal(_expectedWrongArchRule6ErrorMessage, exception6.Message);
+            Assert.Equal(_expectedWrongArchRule7ErrorMessage, exception7.Message);
+            Assert.Equal(_expectedWrongArchRule8ErrorMessage, exception8.Message);
+            Assert.Equal(_expectedWrongArchRule1AndWrongArchRule3ErrorMessage, exception1And3.Message);
+            Assert.Equal(_expectedWrongArchRule4AndWrongArchRule8ErrorMessage, exception4And8.Message);
+        }
+
 
         [Fact]
         public void CreateErrorMessageTest()
         {
             Assert.Equal(ExpectedTrueArchRuleErrorMessage, TrueArchRule1.Evaluate(Architecture).ToErrorMessage());
-            Assert.Equal(ExpectedTrueArchRuleErrorMessage, TrueArchRule1.Evaluate(Architecture).ToErrorMessage());
+            Assert.Equal(ExpectedTrueArchRuleErrorMessage, TrueArchRule2.Evaluate(Architecture).ToErrorMessage());
             Assert.Equal(_expectedWrongArchRule1ErrorMessage, WrongArchRule1.Evaluate(Architecture).ToErrorMessage());
             Assert.Equal(_expectedWrongArchRule2ErrorMessage, WrongArchRule2.Evaluate(Architecture).ToErrorMessage());
             Assert.Equal(_expectedWrongArchRule3ErrorMessage, WrongArchRule3.Evaluate(Architecture).ToErrorMessage());
