efcee89ac09c36e38a82b35c0374791f7e6b5815
Fritz Brandhuber <fritz.brandhuber@tngtech.com>
2019-09-30 17:32:46 +0200

    Moved ArchRuleAsserts and FailedArchRuleException in the project folder to add it to git.
    Signed-off-by: Fritz Brandhuber <fritz.brandhuber@tngtech.com>

---
+++ XunitExtension/Asserts/ArchRuleAsserts.cs
@@ -0,0 +1,19 @@
+﻿using ArchUnitNET.Domain;
+using ArchUnitNET.Fluent;
+using Xunit.Sdk;
+
+
+// ReSharper disable once CheckNamespace
+namespace Xunit
+{
+    partial class Assert
+    {
+        public static void ArchRule(Architecture architecture, IArchRule archRule)
+        {
+            if (!architecture.ViolatesRule(archRule))
+            {
+                throw new FailedArchRuleException(architecture, archRule);
+            }
+        }
+    }
+}
---
+++ XunitExtension/Asserts/Sdk/Exceptions/FailedArchRuleException.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using ArchUnitNET.Domain;
+using ArchUnitNET.Fluent;
+using ArchUnitNET.Fluent.Extensions;
+
+// ReSharper disable once CheckNamespace
+namespace Xunit.Sdk
+{
+    public class FailedArchRuleException : XunitException
+    {
+        public FailedArchRuleException(Architecture architecture, IArchRule archRule)
+            : this(architecture.EvaluateRule(archRule))
+        {
+        }
+
+        public FailedArchRuleException(IEnumerable<EvaluationResult> evaluationResults)
+            : base(evaluationResults.ToErrorMessage())
+        {
+        }
+    }
+}
