49fb71a9723c66add5afb5cb5d938fb0c2f8556f
Fritz Brandhuber <fritz.brandhuber@tngtech.com>
2019-10-08 11:42:59 +0200

    Added CheckRule() extension for Architecture
    Signed-off-by: Fritz Brandhuber <fritz.brandhuber@tngtech.com>

--- XunitExtension/ArchRuleExtensions.cs
+++ XunitExtension/ArchRuleExtensions.cs
@@ -15,5 +15,15 @@ namespace Xunit
         {
             Assert.ArchRule(architecture, archRule);
         }
+
+        /// <summary>
+        ///     Verifies that the architecture meets the criteria of the archrule.
+        /// </summary>
+        /// <param name="architecture">The architecture to be tested</param>
+        /// <param name="archRule">The rule to test the architecture with</param>
+        public static void CheckRule(this Architecture architecture, IArchRule archRule)
+        {
+            Assert.ArchRule(architecture, archRule);
+        }
     }
 }
