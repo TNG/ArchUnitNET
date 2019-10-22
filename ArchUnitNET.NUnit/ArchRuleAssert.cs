41a49d632d5283b3cd82caeec06c9d93b1a45cf8
Fritz Brandhuber <fritz.brandhuber@tngtech.com>
2019-10-22 13:59:58 +0200

    Implemented extension methods for NUnit
    Signed-off-by: Fritz Brandhuber <fritz.brandhuber@tngtech.com>

---
+++ ArchUnitNET.NUnit/ArchRuleAssert.cs
@@ -0,0 +1,23 @@
+﻿using ArchUnitNET.Domain;
+using ArchUnitNET.Fluent;
+using ArchUnitNET.Fluent.Extensions;
+using NUnit.Framework;
+
+namespace ArchUnitNET.NUnit
+{
+    public static class ArchRuleAssert
+    {
+        /// <summary>
+        ///     Verifies that the architecture meets the criteria of the archrule.
+        /// </summary>
+        /// <param name="architecture">The architecture to be tested</param>
+        /// <param name="archRule">The rule to test the architecture with</param>
+        public static void FulfilsRule(Architecture architecture, IArchRule archRule)
+        {
+            if (!architecture.FulfilsRule(archRule))
+            {
+                Assert.Fail(architecture.EvaluateRule(archRule).ToErrorMessage());
+            }
+        }
+    }
+}
---
+++ ArchUnitNET.NUnit/ArchRuleExtensions.cs
@@ -0,0 +1,28 @@
+﻿using ArchUnitNET.Domain;
+using ArchUnitNET.Fluent;
+
+namespace ArchUnitNET.NUnit
+{
+    public static class ArchRuleExtensions
+    {
+        /// <summary>
+        ///     Verifies that the architecture meets the criteria of the archrule.
+        /// </summary>
+        /// <param name="archRule">The rule to test the architecture with</param>
+        /// <param name="architecture">The architecture to be tested</param>
+        public static void Check(this IArchRule archRule, Architecture architecture)
+        {
+            ArchRuleAssert.FulfilsRule(architecture, archRule);
+        }
+
+        /// <summary>
+        ///     Verifies that the architecture meets the criteria of the archrule.
+        /// </summary>
+        /// <param name="architecture">The architecture to be tested</param>
+        /// <param name="archRule">The rule to test the architecture with</param>
+        public static void CheckRule(this Architecture architecture, IArchRule archRule)
+        {
+            ArchRuleAssert.FulfilsRule(architecture, archRule);
+        }
+    }
+}
