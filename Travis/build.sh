0469a9f24d712fc596a5f58eceef8cc467238066
Fritz Brandhuber <fritz.brandhuber@tngtech.com>
2019-10-22 19:24:34 +0200

    Assign Build Version by git tag. Extracted Travis build script in it's own file
    Signed-off-by: Fritz Brandhuber <fritz.brandhuber@tngtech.com>

---
+++ Travis/build.sh
@@ -0,0 +1,6 @@
+﻿#!/usr/bin/env bash
+set -ev
+dotnet restore
+dotnet build -c Release
+[[ -z "$TRAVIS_TAG" ]] && dotnet pack -c Release --output nupkgs -p:PackageVersion="0.0.0" || dotnet pack -c Release --output nupkgs -p:PackageVersion="$TRAVIS_TAG"
+dotnet test -c Release
