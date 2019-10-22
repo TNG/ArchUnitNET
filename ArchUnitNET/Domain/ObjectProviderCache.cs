17d9839156e89323f9d9b220957b92b9b05d2ee0
Fritz Brandhuber <fritz.brandhuber@tngtech.com>
2019-10-22 17:01:30 +0200

    Changed dynamic dictionary to use object instead. Removed reference to Microsoft.CSharp as it was only needed for the dynamic dictionary.
    Signed-off-by: Fritz Brandhuber <fritz.brandhuber@tngtech.com>

--- ArchUnitNET/Domain/ObjectProviderCache.cs
+++ ArchUnitNET/Domain/ObjectProviderCache.cs
@@ -7,23 +7,27 @@ namespace ArchUnitNET.Domain
     public class ObjectProviderCache
     {
         private readonly Architecture _architecture;
-        private readonly Dictionary<dynamic, dynamic> _cache;
+        private readonly Dictionary<int, object> _cache;
 
         public ObjectProviderCache(Architecture architecture)
         {
             _architecture = architecture;
-            _cache = new Dictionary<dynamic, dynamic>();
+            _cache = new Dictionary<int, object>();
         }
 
         public IEnumerable<T> GetOrCreateObjects<T>(IObjectProvider<T> objectProvider,
             Func<Architecture, IEnumerable<T>> providingFunction) where T : ICanBeAnalyzed
         {
-            if (!_cache.ContainsKey(objectProvider))
+            unchecked
             {
-                _cache.Add(objectProvider, providingFunction(_architecture));
-            }
+                var key = (objectProvider.GetHashCode() * 397) ^ objectProvider.GetType().GetHashCode();
+                if (!_cache.ContainsKey(key))
+                {
+                    _cache.Add(key, providingFunction(_architecture));
+                }
 
-            return _cache[objectProvider];
+                return (IEnumerable<T>) _cache[key];
+            }
         }
     }
 }
