f60f3a6a62d122cc5fa8ad3ab7d63d290fd372dd
Fritz Brandhuber <fritz.brandhuber@tngtech.com>
2019-11-07 16:45:57 +0100

    Set Assembly Version automatically with Travis. Changed LoadAssemblies() Test
    Signed-off-by: Fritz Brandhuber <fritz.brandhuber@tngtech.com>
    Signed-off-by: Marco Studer <marco.studer@bison-group.com>

--- ArchUnitNETTests/Core/ArchLoaderTests.cs
+++ ArchUnitNETTests/Core/ArchLoaderTests.cs
@@ -5,47 +5,19 @@
 // 	SPDX-License-Identifier: Apache-2.0
 
 using System.Linq;
-using ArchUnitNET.Domain;
 using Xunit;
 using static ArchUnitNETTests.Fluent.Extensions.StaticTestArchitectures;
-
-// ReSharper disable InconsistentNaming
 
 namespace ArchUnitNETTests.Core
 {
     public class ArchLoaderTests
     {
-        private static readonly Assembly ArchUnitNETAssembly =
-            new Assembly("ArchUnitNET, Version=0.2.0.0, Culture=neutral, PublicKeyToken=null",
-                "ArchUnitNET, Version=0.2.0.0, Culture=neutral, PublicKeyToken=null", false);
-
-        private static readonly Assembly ArchUnitNETTestsAssembly =
-            new Assembly("ArchUnitNETTests, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null",
-                "ArchUnitNETTests, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", false);
-
-        private static readonly Assembly ArchUnitTestAssemblyAssembly =
-            new Assembly("TestAssembly, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null",
-                "TestAssembly, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", false);
-
-        private static readonly Assembly XunitExtensionAssembly =
-            new Assembly("ArchUnitNET.xUnit, Version=0.2.0.0, Culture=neutral, PublicKeyToken=null",
-                "ArchUnitNET.xUnit, Version=0.2.0.0, Culture=neutral, PublicKeyToken=null", false);
-
         [Fact]
-        public void LoadAssembliesCorrectly()
+        public void LoadAssemblies()
         {
             Assert.Equal(4, FullArchUnitNETArchitecture.Assemblies.Count());
-            Assert.Contains(ArchUnitNETAssembly, FullArchUnitNETArchitecture.Assemblies);
-            Assert.Contains(ArchUnitNETTestsAssembly, FullArchUnitNETArchitecture.Assemblies);
-            Assert.Contains(ArchUnitTestAssemblyAssembly, FullArchUnitNETArchitecture.Assemblies);
-            Assert.Contains(XunitExtensionAssembly, FullArchUnitNETArchitecture.Assemblies);
-
             Assert.Single(ArchUnitNETTestArchitecture.Assemblies);
-            Assert.Contains(ArchUnitNETTestsAssembly, ArchUnitNETTestArchitecture.Assemblies);
-
             Assert.Single(ArchUnitNETTestAssemblyArchitecture.Assemblies);
-            Assert.Contains(ArchUnitTestAssemblyAssembly, ArchUnitNETTestAssemblyArchitecture.Assemblies);
-
             Assert.NotEmpty(FullArchUnitNETArchitectureWithDependencies.Assemblies);
             Assert.NotEmpty(ArchUnitNETTestArchitectureWithDependencies.Assemblies);
             Assert.NotEmpty(ArchUnitNETTestAssemblyArchitectureWithDependencies.Assemblies);
