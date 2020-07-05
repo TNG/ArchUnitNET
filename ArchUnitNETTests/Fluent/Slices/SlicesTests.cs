6b59cde4b38e89942ea80d8f3ca9aa089ef76c7a
Fritz Brandhuber <fritz.brandhuber@tngtech.com>
2020-07-05 20:26:37 +0200

    implement tests for NotDependOnEachOther()
    Signed-off-by: Fritz Brandhuber <fritz.brandhuber@tngtech.com>
    Signed-off-by: Marco Studer <marco.studer@bison-group.com>

--- ArchUnitNETTests/Fluent/Slices/SlicesTests.cs
+++ ArchUnitNETTests/Fluent/Slices/SlicesTests.cs
@@ -29,11 +29,25 @@ namespace ArchUnitNETTests.Fluent.Slices
         [Fact]
         public void MatchingTest()
         {
-            Assert.True(SliceRuleDefinition.Slices().Matching("TestAssembly.Slices.(*)").GetSlices(StaticTestArchitectures.ArchUnitNETTestAssemblyArchitecture).Count() == 3);
-            Assert.True(SliceRuleDefinition.Slices().Matching("TestAssembly.Slices.(**)").GetSlices(StaticTestArchitectures.ArchUnitNETTestAssemblyArchitecture).Count() == 5);
-            Assert.True(SliceRuleDefinition.Slices().Matching("TestAssembly.Slices.(*)..").GetSlices(StaticTestArchitectures.ArchUnitNETTestAssemblyArchitecture).Count() == 3);
-            Assert.True(SliceRuleDefinition.Slices().Matching("TestAssembly.Slices.(**)..").GetSlices(StaticTestArchitectures.ArchUnitNETTestAssemblyArchitecture).Count() == 3);
+            Assert.Equal(3, SliceRuleDefinition.Slices().Matching("TestAssembly.Slices.(*)").GetSlices(StaticTestArchitectures.ArchUnitNETTestAssemblyArchitecture).Count());
+            Assert.Equal(7, SliceRuleDefinition.Slices().Matching("TestAssembly.Slices.(**)").GetSlices(StaticTestArchitectures.ArchUnitNETTestAssemblyArchitecture).Count());
+            Assert.Equal(3, SliceRuleDefinition.Slices().Matching("TestAssembly.Slices.(*)..").GetSlices(StaticTestArchitectures.ArchUnitNETTestAssemblyArchitecture).Count());
+            Assert.Equal(3, SliceRuleDefinition.Slices().Matching("TestAssembly.Slices.(**)..").GetSlices(StaticTestArchitectures.ArchUnitNETTestAssemblyArchitecture).Count());
+            Assert.Equal(2, SliceRuleDefinition.Slices().Matching("TestAssembly.Slices.Slice3.(*)").GetSlices(StaticTestArchitectures.ArchUnitNETTestAssemblyArchitecture).Count());
             Assert.False(SliceRuleDefinition.Slices().Matching("TestAssembly.Slices.Service.(*)").GetSlices(StaticTestArchitectures.ArchUnitNETTestAssemblyArchitecture).Any());
+        }
+
+        [Fact]
+        public void NotDependOnEachOtherTest()
+        {
+            SliceRuleDefinition.Slices().Matching("TestAssembly.Slices.Slice3.(*)").Should().NotDependOnEachOther().Check(StaticTestArchitectures.ArchUnitNETTestAssemblyArchitecture);
+            SliceRuleDefinition.Slices().Matching("TestAssembly.Slices.Slice1.(*)").Should().NotDependOnEachOther().Check(StaticTestArchitectures.ArchUnitNETTestAssemblyArchitecture);
+            Assert.True(SliceRuleDefinition.Slices().Matching("TestAssembly.Slices.Slice1.(*)").Should().NotDependOnEachOther().HasNoViolations(StaticTestArchitectures.ArchUnitNETTestAssemblyArchitecture));
+            SliceRuleDefinition.Slices().Matching("TestAssembly.Slices.(**)").Should().NotDependOnEachOther().Check(StaticTestArchitectures.ArchUnitNETTestAssemblyArchitecture);
+            Assert.Throws<FailedArchRuleException>(() => SliceRuleDefinition.Slices().Matching("TestAssembly.Slices.(**)").Should().NotDependOnEachOther().Check(StaticTestArchitectures.ArchUnitNETTestAssemblyArchitecture));
+            Assert.False(SliceRuleDefinition.Slices().Matching("TestAssembly.Slices.(**)").Should().NotDependOnEachOther().HasNoViolations(StaticTestArchitectures.ArchUnitNETTestAssemblyArchitecture));
+            Assert.False(SliceRuleDefinition.Slices().Matching("TestAssembly.Slices.(*)..").Should().NotDependOnEachOther().HasNoViolations(StaticTestArchitectures.ArchUnitNETTestAssemblyArchitecture));
+            Assert.False(SliceRuleDefinition.Slices().Matching("TestAssembly.Slices.(**)..").Should().NotDependOnEachOther().HasNoViolations(StaticTestArchitectures.ArchUnitNETTestAssemblyArchitecture));
         }
     }
 }
