7f65faf0b55786dfda7ad1c722413048be2bf649
Fritz Brandhuber <fritz.brandhuber@tngtech.com>
2020-07-05 20:06:28 +0200

    implement NotDependOnEachOther()
    Signed-off-by: Fritz Brandhuber <fritz.brandhuber@tngtech.com>
    Signed-off-by: Marco Studer <marco.studer@bison-group.com>

--- ArchUnitNET/Fluent/Slices/SlicesShould.cs
+++ ArchUnitNET/Fluent/Slices/SlicesShould.cs
@@ -5,7 +5,6 @@
 // 	SPDX-License-Identifier: Apache-2.0
 // 
 
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using ArchUnitNET.Domain;
@@ -38,7 +37,7 @@ namespace ArchUnitNET.Fluent.Slices
 
                 return from slice in slicesList
                     let passed = cycles.SelectMany(cycle => cycle.Contents).Contains(slice)
-                    let description = slice.Description + (passed ? " does have" : " does not have") + " cyclic dependencies."
+                    let description = slice.Description + (passed ? " does not have" : " does have") + " cyclic dependencies."
                     select new EvaluationResult(slice, passed, description, archRule, architecture);
             }
 
@@ -49,7 +48,27 @@ namespace ArchUnitNET.Fluent.Slices
 
         public SliceRule NotDependOnEachOther()
         {
-            throw new NotImplementedException();
+            IEnumerable<EvaluationResult> Evaluate(IEnumerable<Slice> slices, ICanBeEvaluated archRule, Architecture architecture)
+            {
+                var slicesList = slices.ToList();
+
+                IEnumerable<Slice> FindDependencies(Slice slice)
+                {
+                    var typeDependencies = slice.Dependencies.Select(dependency => dependency.Target).Distinct();
+                    return typeDependencies.Select(type => slicesList.FirstOrDefault(slc => slc.Types.Contains(type))).Where(slc => slc != null);
+                }
+
+                return from slice in slicesList
+                    let dependencies = FindDependencies(slice)
+                    let passed = !dependencies.Any()
+                    let description = slice.Description + (passed
+                        ? " does not depend on another slice."
+                        : " does depend on " + dependencies.First().Description + dependencies.Skip(1).Aggregate("", (str, dependency) => str + " and " + dependency.Description)) + "."
+                    select new EvaluationResult(slice, passed, description, archRule, architecture);
+            }
+
+            _ruleCreator.SetEvaluationFunction(Evaluate);
+            _ruleCreator.AddToDescription("not depend on each other");
             return new SliceRule(_ruleCreator);
         }
     }
