2f34cf52accf37f06cb72189bcd59d67861fda73
Fritz Brandhuber <fritz.brandhuber@tngtech.com>
2020-07-21 17:12:39 +0200

    only show unique dependencies in description of failed slice rules
    Signed-off-by: Fritz Brandhuber <fritz.brandhuber@tngtech.com>

---
+++ ArchUnitNET/Domain/TypeDependencyComparer.cs
@@ -0,0 +1,33 @@
+﻿//  Copyright 2019 Florian Gather <florian.gather@tngtech.com>
+// 	Copyright 2019 Paula Ruiz <paularuiz22@gmail.com>
+// 	Copyright 2019 Fritz Brandhuber <fritz.brandhuber@tngtech.com>
+// 
+// 	SPDX-License-Identifier: Apache-2.0
+// 
+
+using System.Collections.Generic;
+using ArchUnitNET.Domain.Dependencies.Types;
+
+namespace ArchUnitNET.Domain
+{
+    public class TypeDependencyComparer : IEqualityComparer<ITypeDependency>
+    {
+        public bool Equals(ITypeDependency x, ITypeDependency y)
+        {
+            if (ReferenceEquals(x, y))
+            {
+                return true;
+            }
+
+            return x != null && y != null && Equals(x.Origin, y.Origin) && Equals(x.Target, y.Target);
+        }
+
+        public int GetHashCode(ITypeDependency obj)
+        {
+            unchecked
+            {
+                return (obj.Origin.GetHashCode() * 397) ^ obj.Target.GetHashCode();
+            }
+        }
+    }
+}
