a08ad5bc6121da047b8e9af26dbb93d6c1d4f129
GeorgKreuzmayr <g.kreuzmayr@gmail.com>
2020-10-05 09:56:18 +0200

    Fix relative path, remove tag
    Signed-off-by: GeorgKreuzmayr <g.kreuzmayr@gmail.com>

--- Travis/deploy.ps1
+++ Travis/deploy.ps1
@@ -1,9 +1,8 @@
 param(
 [string]$apiKey,
-[string]$source,
-[string]$tag
+[string]$source
 )
 
-dotnet nuget push ./ArchUnitNET/nupkgs/TngTech.ArchUnitNET.*.nupkg -k $apiKey -s $source
-dotnet nuget push ./ArchUnitNET.xUnit/nupkgs/TngTech.ArchUnitNET.xUnit.*.nupkg -k $apiKey -s $source
-dotnet nuget push ./ArchUnitNET.NUnit/nupkgs/TngTech.ArchUnitNET.NUnit.*.nupkg -k $apiKey -s $source
+dotnet nuget push ($PWD.Path + "\ArchUnitNET\nupkgs\TngTech.ArchUnitNET.*.nupkg") -k $apiKey -s $source
+dotnet nuget push ($PWD.Path + "\ArchUnitNET.xUnit\nupkgs\TngTech.ArchUnitNET.xUnit.*.nupkg") -k $apiKey -s $source
+dotnet nuget push ($PWD.Path + "\ArchUnitNET.NUnit\nupkgs\TngTech.ArchUnitNET.NUnit.*.nupkg") -k $apiKey -s $source
