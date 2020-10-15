045f0aeeb760ad0dd06848dd9968527811fb1f41
Fritz Brandhuber <fritz.brandhuber@tngtech.com>
2020-10-15 17:21:37 +0200

    add cast tests
    Signed-off-by: Fritz Brandhuber <fritz.brandhuber@tngtech.com>
    Signed-off-by: Marco Studer <marco.studer@bison-group.com>

---
+++ ArchUnitNETTests/Dependencies/CastDependenciesTest.cs
@@ -0,0 +1,65 @@
+﻿//  Copyright 2019 Florian Gather <florian.gather@tngtech.com>
+// 	Copyright 2019 Fritz Brandhuber <fritz.brandhuber@tngtech.com>
+// 	Copyright 2020 Pavel Fischer <rubbiroid@gmail.com>
+// 
+// 	SPDX-License-Identifier: Apache-2.0
+// 
+
+using System.Linq;
+using ArchUnitNET.Domain;
+using ArchUnitNET.Domain.Extensions;
+using ArchUnitNET.Loader;
+using Xunit;
+
+namespace ArchUnitNETTests.Dependencies
+{
+    public class CastDependenciesTest
+    {
+        private static readonly Architecture Architecture =
+            new ArchLoader().LoadAssembly(typeof(ClassWithCastDependency).Assembly).Build();
+
+        private readonly Class _castClassA;
+        private readonly Class _castClassB;
+        private readonly Interface _castInterfaceA;
+        private readonly Class _classWithCastDependency;
+
+        public CastDependenciesTest()
+        {
+            _castClassA = Architecture.GetClassOfType(typeof(CastClassA));
+            _castClassB = Architecture.GetClassOfType(typeof(CastClassB));
+            _castInterfaceA = Architecture.GetInterfaceOfType(typeof(ICastInterfaceA));
+            _classWithCastDependency = Architecture.GetClassOfType(typeof(ClassWithCastDependency));
+        }
+
+        [Fact]
+        public void CastTest()
+        {
+            var typeDependencies = _classWithCastDependency.GetTypeDependencies(Architecture).ToList();
+
+            Assert.Contains(_castClassA, typeDependencies);
+            Assert.Contains(_castClassB, typeDependencies);
+            Assert.Contains(_castInterfaceA, typeDependencies);
+        }
+    }
+
+    public class ClassWithCastDependency
+    {
+        public ClassWithCastDependency()
+        {
+            var type = (CastClassA) new CastClassB();
+            var type2 = (ICastInterfaceA) new CastClassB();
+        }
+    }
+
+    public class CastClassA
+    {
+    }
+
+    public interface ICastInterfaceA
+    {
+    }
+
+    public class CastClassB : CastClassA, ICastInterfaceA
+    {
+    }
+}
