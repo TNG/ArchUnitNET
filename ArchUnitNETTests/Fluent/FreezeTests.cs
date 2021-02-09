8898c2d98c5323ca498f47a4b7f7104f0f3108b9
Fritz Brandhuber <fritz.brandhuber@tngtech.com>
2021-02-09 18:20:39 +0100

    test for frozen rules
    Signed-off-by: Fritz Brandhuber <fritz.brandhuber@tngtech.com>
    Signed-off-by: Marco Studer <marco.studer@bison-group.com>

---
+++ ArchUnitNETTests/Fluent/FreezeTests.cs
@@ -0,0 +1,34 @@
+﻿//  Copyright 2019 Florian Gather <florian.gather@tngtech.com>
+// 	Copyright 2019 Fritz Brandhuber <fritz.brandhuber@tngtech.com>
+// 	Copyright 2020 Pavel Fischer <rubbiroid@gmail.com>
+// 
+// 	SPDX-License-Identifier: Apache-2.0
+// 
+
+using ArchUnitNET.Domain;
+using ArchUnitNET.Fluent;
+using ArchUnitNET.Loader;
+using Xunit;
+using static ArchUnitNET.Fluent.ArchRuleDefinition;
+using static ArchUnitNET.Fluent.Freeze.FreezingArchRule;
+
+namespace ArchUnitNETTests.Fluent
+{
+    public class FreezeTests
+    {
+        private static readonly Architecture Architecture =
+            new ArchLoader().LoadAssembly(typeof(FreezeTests).Assembly).Build();
+
+        private readonly IArchRule _failingRule = Types().That().Are(typeof(Violation)).Should().NotBePrivate();
+
+        [Fact]
+        public void PassFrozenRule()
+        {
+            Freeze(_failingRule).Check(Architecture);
+        }
+
+        private class Violation
+        {
+        }
+    }
+}
