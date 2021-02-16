f84903d888c6f28a2411808f22154f15d64754e9
studix <marco.studer@bison-group.com>
2021-02-16 21:00:57 +0100

    add unit test for ClassDiagramAssociation
    Signed-off-by: Marco Studer <marco.studer@bison-group.com>

---
+++ TestAssembly/Diagram/SimpleDependency/Origin/SomeOriginClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestAssembly.Diagram.SimpleDependency.Target;
+
+namespace TestAssembly.Diagram.SimpleDependency.Origin
+{
+    public class SomeOriginClass
+    {
+        internal void AccessTarget()
+        {
+            new SomeTargetClass();
+        }
+    }
+}
