4d7c382451e4824e325d3948d3a0257c61fd416e
studix <marco.studer@bison-group.com>
2021-03-05 06:48:33 +0100

    suppress compiler warning in test assembly
    Signed-off-by: Marco Studer <marco.studer@bison-group.com>

--- TestAssembly/Diagram/NoDependencies/SomeNamespace/IndependentOrigin.cs
+++ TestAssembly/Diagram/NoDependencies/SomeNamespace/IndependentOrigin.cs
@@ -6,6 +6,8 @@ namespace TestAssembly.Diagram.NoDependencies.SomeNamespace
 {
     public class IndependentOrigin
     {
-        private DependencyWithinNamespace legalField;
+        #pragma warning disable CS0169
+        private readonly DependencyWithinNamespace _legalField;
+        #pragma warning restore CS0169
     }
 }
