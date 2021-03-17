3537ff206f3beb61e9e36ef157e459c23a040957
Georg Kreuzmayr <g.kreuzmayr@gmail.com>
2021-03-17 19:59:09 +0100

    Fix path in deploy_documentation.sh and remove condition
    Signed-off-by: Georg Kreuzmayr <g.kreuzmayr@gmail.com>
    Signed-off-by: Marco Studer <marco.studer@bison-group.com>

--- Travis/deploy_documentation.sh
+++ Travis/deploy_documentation.sh
@@ -2,8 +2,6 @@
 
 # Generate the docs only if master, the travis_build_docs is true and we can use secure variables
 
-if [[ "$TRAVIS_BRANCH" = "master" && "$TRAVIS_PULL_REQUEST" = "false" ]] ; then
-  chmod +x ./Travis/setup-ssh.sh ./Travis/generate-documentation.sh
-  source ./Travis/setup-ssh.sh || travis_terminate 1
-  ./Travis/generate-documentation.sh || travis_terminate 1
-fi
+chmod +x "$TRAVIS_BUILD_DIR/Travis/setup-ssh.sh" "$TRAVIS_BUILD_DIR./Travis/generate-documentation.sh"
+source ./Travis/setup-ssh.sh || travis_terminate 1
+./Travis/generate-documentation.sh || travis_terminate 1
