296626671d51e16ce9aa31045668b80fe3385037
Georg Kreuzmayr <g.kreuzmayr@gmail.com>
2021-03-17 19:21:46 +0100

    Remove encryption of ssh key, use Travis variable
    Signed-off-by: Georg Kreuzmayr <g.kreuzmayr@gmail.com>

--- Travis/setup-ssh.sh
+++ Travis/setup-ssh.sh
@@ -5,8 +5,7 @@ set -ev
 declare -r SSH_FILE="$(mktemp -u $HOME/.ssh/travis_temp_ssh_key_XXXX)"
 # Decrypt the file containing the private key (put the real name of the variables)
 
-openssl aes-256-cbc -k "$travis_key_password" \
- -d -md sha256 -a -in "Travis/travis_key.enc" -out "$SSH_FILE"
+echo "$SSH_PRIVATE" > $SSH_FILE
 
 # Enable SSH authentication
 chmod 600 "$SSH_FILE" \
