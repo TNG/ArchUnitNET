4e54be5481217f5487ee380d56f050fc6d1548dc
Marco Studer <marco.studer@bison-group.com>
2021-03-29 22:18:44 +0200

    add more diagram tests
    Signed-off-by: Marco Studer <marco.studer@bison-group.com>

--- ArchUnitNETTests/Domain/PlantUml/ClassDiagramAssociationTest.cs
+++ ArchUnitNETTests/Domain/PlantUml/ClassDiagramAssociationTest.cs
@@ -9,6 +9,8 @@ using System.Text.RegularExpressions;
 using TestAssembly.Diagram.SimpleDependency.Origin;
 using ArchUnitNET.Domain.Extensions;
 using Xunit;
+using TestAssembly;
+using TestAssembly.Diagram.ConfusingNamespaceNames.FooNamespace.BarNamespace;
 
 namespace ArchUnitNETTests.Domain.PlantUml
 {
@@ -27,6 +29,77 @@ namespace ArchUnitNETTests.Domain.PlantUml
 
             Class clazz = Architecture.GetClassOfType(typeof(SomeOriginClass));
             Assert.Equal(expectedNamespaceIdentifier, classDiagramAssociation.GetNamespaceIdentifiersFromComponentOf(clazz).Single());
+        }
+
+        [Fact]
+        public void GetTargetNamespaceIdentifierOfClass()
+        {
+            string expectedTarget1 = ".*.Target1";
+            string expectedTarget2 = ".*.Target2";
+            
+            ClassDiagramAssociation classDiagramAssociation = CreateAssociation(TestDiagram.In(Path.GetTempPath())
+                    .Component("A").WithStereoTypes(Regex.Replace(typeof(SomeOriginClass).Namespace, @".*\.", ".*."))
+                    .Component("B").WithStereoTypes(expectedTarget1)
+                    .Component("C").WithStereoTypes(expectedTarget2)
+                    .DependencyFrom("[A]").To("[B]")
+                    .DependencyFrom("[A]").To("[C]")
+                    .Write());
+
+            Class clazz = Architecture.GetClassOfType(typeof(SomeOriginClass));
+
+            Assert.Equal(new[] { expectedTarget1, expectedTarget2 }, classDiagramAssociation.GetTargetNamespaceIdentifiers(clazz).OrderBy(s => s).ToList());
+        }
+
+        [Fact]
+        public void RejectsClassNotContainedInAnyComponent()
+        {
+            ClassDiagramAssociation classDiagramAssociation = CreateAssociation(TestDiagram.In(Path.GetTempPath())
+                    .Component("SomeComponent").WithStereoTypes(".*.SomeStereotype.")
+                    .Write());
+
+            Class classNotContained = Architecture.GetClassOfType(typeof(object));
+
+            InvalidOperationException exception = Assert.Throws<InvalidOperationException>(() => classDiagramAssociation.GetTargetNamespaceIdentifiers(classNotContained));
+            Assert.Equal(string.Format("Class {0} is not contained in any component", typeof(object).Name), exception.Message);
+        }
+
+        [Fact]
+        public void ReportsIfClassIsContainedInAnyComponent()
+        {
+            ClassDiagramAssociation classDiagramAssociation = CreateAssociation(TestDiagram.In(Path.GetTempPath())
+                    .Component("Object").WithStereoTypes(typeof(object).Namespace)
+                    .Write());
+
+            Assert.True(classDiagramAssociation.Contains(Architecture.GetClassOfType(typeof(object))), "association contains " + typeof(object).Name);
+            Assert.False(classDiagramAssociation.Contains(Architecture.GetClassOfType(typeof(Class2))), "association contains " + typeof(Class2).Name);
+        }
+
+        [Fact]
+        public void ClassResidesInMultipleNamespaces()
+        {
+            ClassDiagramAssociation classDiagramAssociation = CreateAssociation(TestDiagram.In(Path.GetTempPath())
+                    .Component("A").WithStereoTypes(".*.FooNamespace.*")
+                    .Component("B").WithStereoTypes(".*.BarNamespace.*")
+                    .Write());
+
+            Class classContainedInTwoComponents = Architecture.GetClassOfType(typeof(ClassInFooAndBarNamespace));
+
+            ComponentIntersectionException exception = Assert.Throws<ComponentIntersectionException>(() => classDiagramAssociation.GetTargetNamespaceIdentifiers(classContainedInTwoComponents));
+            Assert.Equal(string.Format("Class {0} may not be contained in more than one component, but is contained in [A, B]", typeof(ClassInFooAndBarNamespace).Name), exception.Message);
+        }
+
+        [Fact]
+        public void RejectsDuplicateStereotype()
+        {
+            string file = TestDiagram.In(Path.GetTempPath())
+                    .Component("first").WithStereoTypes(".*.Identical.*")
+                    .Component("second").WithStereoTypes(".*.Identical.*")
+                    .Write();
+
+            Class classContainedInTwoComponents = Architecture.GetClassOfType(typeof(ClassInFooAndBarNamespace));
+
+            IllegalDiagramException exception = Assert.Throws<IllegalDiagramException>(() => CreateAssociation(file));
+            Assert.Equal("Stereotype '.*.Identical.*' should be unique", exception.Message);
         }
 
         private ClassDiagramAssociation CreateAssociation(string file)
