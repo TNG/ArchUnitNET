bb333d893fa8a1743802233158119fab0bbd1169
Marco Studer <marco.studer@bison-group.com>
2021-04-18 20:03:24 +0200

    add example test for plant uml
    Signed-off-by: Marco Studer <marco.studer@bison-group.com>

---
+++ ExampleTest/PlantUml/Customers/Customer.cs
@@ -0,0 +1,20 @@
+﻿using ExampleTest.PlantUml.Addresses;
+using ExampleTest.PlantUml.Orders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExampleTest.PlantUml.Customers
+{
+    public class Customer
+    {
+        public Address Address { get; set; }
+
+        internal void AddOrder(Order order)
+        {
+            // simply having such a parameter violates the specified UML diagram
+        }
+
+    }
+
+}
---
+++ ExampleTest/PlantUml/Products/Product.cs
@@ -0,0 +1,31 @@
+﻿using ExampleTest.PlantUml.Customers;
+using ExampleTest.PlantUml.Orders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExampleTest.PlantUml.Products
+{
+    public class Product
+    {
+        public Customer _customer;
+
+        internal Order Order
+        {
+            get
+            {
+                return null; // the return type violates the specified UML diagram
+            }
+        }
+
+
+        public void Register()
+        {
+        }
+
+        public void Report()
+        {
+        }
+    }
+
+}
