7a1a849b47ff1253c358e8f6d48db3097b838d65
Fritz Brandhuber <fritz.brandhuber@tngtech.com>
2021-04-28 00:16:30 +0200

    test to ensure that references to static methods in generators are treated the same as in other methods
    Signed-off-by: Fritz Brandhuber <fritz.brandhuber@tngtech.com>

--- ArchUnitNETTests/Dependencies/DependenciesToStaticMethodsTests.cs
+++ ArchUnitNETTests/Dependencies/DependenciesToStaticMethodsTests.cs
@@ -5,6 +5,8 @@
 // 	SPDX-License-Identifier: Apache-2.0
 // 
 
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using ArchUnitNET.Domain;
 using ArchUnitNET.Domain.Extensions;
@@ -19,15 +21,25 @@ namespace ArchUnitNETTests.Dependencies
             new ArchLoader().LoadAssembly(typeof(DependenciesToStaticMethodsTests).Assembly).Build();
 
         private readonly Class _accessingClass;
+        private readonly Class _classAccessingStaticMethodInGenerator;
+        private readonly Class _classAccessingStaticMethodWithoutGenerator;
         private readonly Class _classWithStaticMethod;
-        private readonly MethodMember _staticMethodMember;
+        private readonly MethodMember _staticMethodMemberWithBody;
+        private readonly FieldMember _staticMethodMemberAsField;
 
 
         public DependenciesToStaticMethodsTests()
         {
             _accessingClass = Architecture.GetClassOfType(typeof(ClassAccessingStaticMethod));
-            _classWithStaticMethod = Architecture.GetClassOfType(typeof(ClassWithStaticMethod));
-            _staticMethodMember = _classWithStaticMethod.GetMethodMembersWithName("StaticMethod(System.Int32)").First();
+            _classAccessingStaticMethodInGenerator =
+                Architecture.GetClassOfType(typeof(ClassAccessingStaticMethodInGenerator));
+            _classAccessingStaticMethodWithoutGenerator =
+                Architecture.GetClassOfType(typeof(ClassAccessingStaticMethodWithoutGenerator));
+            _classWithStaticMethod = Architecture.GetClassOfType(typeof(ClassWithStaticMethods));
+            _staticMethodMemberWithBody = _classWithStaticMethod
+                .GetMethodMembersWithName(nameof(ClassWithStaticMethods.StaticMethodWithBody) + "(System.Int32)").First();
+            _staticMethodMemberAsField = _classWithStaticMethod
+                .GetFieldMembersWithName(nameof(ClassWithStaticMethods.StaticMethodAsField)).First();
         }
 
         [Fact]
@@ -37,26 +49,103 @@ namespace ArchUnitNETTests.Dependencies
             var methodDependencies = _accessingClass.GetCalledMethods().ToList();
 
             Assert.Contains(_classWithStaticMethod, typeDependencies);
-            Assert.Contains(_staticMethodMember, methodDependencies);
+            Assert.Contains(_staticMethodMemberWithBody, methodDependencies);
+        }
+
+        [Fact]
+        public void StaticMethodCallToFieldFound()
+        {
+            var typeDependencies = _accessingClass.GetTypeDependencies().ToList();
+            var fieldDependencies = _accessingClass.GetAccessedFieldMembers().ToList();
+
+            Assert.Contains(_classWithStaticMethod, typeDependencies);
+            Assert.Contains(_staticMethodMemberAsField, fieldDependencies);
+        }
+
+        [Fact]
+        public void StaticReferencesInGeneratorFound()
+        {
+            var typeDependencies = _classAccessingStaticMethodInGenerator.GetTypeDependencies().ToList();
+            var methodDependencies = _classAccessingStaticMethodInGenerator.GetCalledMethods().ToList();
+            var fieldDependencies = _classAccessingStaticMethodInGenerator.GetAccessedFieldMembers().ToList();
+
+            Assert.Contains(_classWithStaticMethod, typeDependencies);
+            Assert.Contains(_staticMethodMemberWithBody, methodDependencies);
+            Assert.Contains(_staticMethodMemberAsField, fieldDependencies);
+        }
+        
+        [Fact]
+        public void StaticReferencesWithoutGeneratorFound()
+        {
+            var typeDependencies = _classAccessingStaticMethodWithoutGenerator.GetTypeDependencies().ToList();
+            var methodDependencies = _classAccessingStaticMethodWithoutGenerator.GetCalledMethods().ToList();
+            var fieldDependencies = _classAccessingStaticMethodWithoutGenerator.GetAccessedFieldMembers().ToList();
+
+            Assert.Contains(_classWithStaticMethod, typeDependencies);
+            Assert.Contains(_staticMethodMemberWithBody, methodDependencies);
+            Assert.Contains(_staticMethodMemberAsField, fieldDependencies);
         }
     }
 
-    internal static class ClassWithStaticMethod
+    internal static class ClassWithStaticMethods
     {
-        public static int StaticMethod(int a)
+        public static int StaticMethodWithBody(int a)
         {
             return a + 1;
         }
+
+        public static Func<int, int> StaticMethodAsField = i => i + 2;
     }
 
 
     internal class ClassAccessingStaticMethod
     {
         private int _a;
+        private int _b;
 
         public ClassAccessingStaticMethod(int a)
         {
-            _a = ClassWithStaticMethod.StaticMethod(a);
+            _a = ClassWithStaticMethods.StaticMethodWithBody(a);
+            _b = ClassWithStaticMethods.StaticMethodAsField(a);
+        }
+    }
+
+    internal class ClassAccessingStaticMethodInGenerator
+    {
+        public IEnumerable<int> Generator(bool useField)
+        {
+            var generatingFunction = useField
+                ? ClassWithStaticMethods.StaticMethodAsField
+                : ClassWithStaticMethods.StaticMethodWithBody;
+
+            var a = 0;
+
+            for (var i = 0; i < 10; i++)
+            {
+                yield return a;
+                a = generatingFunction(a);
+            }
+        }
+    }
+
+    internal class ClassAccessingStaticMethodWithoutGenerator
+    {
+        public IEnumerable<int> WithoutGenerator(bool useField)
+        {
+            var results = new List<int>();
+            var generatingFunction = useField
+                ? ClassWithStaticMethods.StaticMethodAsField
+                : ClassWithStaticMethods.StaticMethodWithBody;
+
+            var a = 0;
+
+            for (var i = 0; i < 10; i++)
+            {
+                results[i] = a;
+                a = generatingFunction(a);
+            }
+
+            return results;
         }
     }
 }
