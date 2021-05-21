abc52420b71ca9e1de349c0133eb004bffaf5860
Florian Gather <florian.gather@tngtech.com>
2021-05-22 00:40:50 +0200

    fix deploy paths

--- Travis/deploy.ps1
+++ Travis/deploy.ps1
@@ -3,7 +3,7 @@ param(
 [string]$source
 )
 
-dotnet nuget push ($PWD.Path + "\ArchUnitNET\nupkgs\TngTech.ArchUnitNET.*.nupkg") -k $apiKey -s $source
-dotnet nuget push ($PWD.Path + "\ArchUnitNET.xUnit\nupkgs\TngTech.ArchUnitNET.xUnit.*.nupkg") -k $apiKey -s $source
-dotnet nuget push ($PWD.Path + "\ArchUnitNET.NUnit\nupkgs\TngTech.ArchUnitNET.NUnit.*.nupkg") -k $apiKey -s $source
-dotnet nuget push ($PWD.Path + "\ArchUnitNET.MSTestV2\nupkgs\TngTech.ArchUnitNET.MSTestV2.*.nupkg") -k $apiKey -s $source
+dotnet nuget push ($PWD.Path + "\nupkgs\TngTech.ArchUnitNET.*.nupkg") -k $apiKey -s $source
+dotnet nuget push ($PWD.Path + "\nupkgs\TngTech.ArchUnitNET.xUnit.*.nupkg") -k $apiKey -s $source
+dotnet nuget push ($PWD.Path + "\nupkgs\TngTech.ArchUnitNET.NUnit.*.nupkg") -k $apiKey -s $source
+dotnet nuget push ($PWD.Path + "\nupkgs\TngTech.ArchUnitNET.MSTestV2.*.nupkg") -k $apiKey -s $source
