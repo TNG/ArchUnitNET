98a9c741410c9e1b47b54dd59c320acd04ec64dc
Fritz Brandhuber <fritz.brandhuber@tngtech.com>
2021-08-25 20:17:20 +0200

    dont push xUnit, NUnit and MSTestV2 package twice (they should all be included in the first command)
    Signed-off-by: Fritz Brandhuber <fritz.brandhuber@tngtech.com>

--- Travis/deploy.ps1
+++ Travis/deploy.ps1
@@ -4,6 +4,3 @@ param(
 )
 
 dotnet nuget push ($PWD.Path + "\nupkgs\TngTech.ArchUnitNET.*.nupkg") -k $apiKey -s $source
-dotnet nuget push ($PWD.Path + "\nupkgs\TngTech.ArchUnitNET.xUnit.*.nupkg") -k $apiKey -s $source
-dotnet nuget push ($PWD.Path + "\nupkgs\TngTech.ArchUnitNET.NUnit.*.nupkg") -k $apiKey -s $source
-dotnet nuget push ($PWD.Path + "\nupkgs\TngTech.ArchUnitNET.MSTestV2.*.nupkg") -k $apiKey -s $source
