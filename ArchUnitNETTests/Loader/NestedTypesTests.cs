9b098b9b89f01c6dd2d1cecfe51e4f1746c1d467
Fritz Brandhuber <fritz.brandhuber@tngtech.com>
2021-09-21 17:39:04 +0200

    add tests for deep nested types
    Signed-off-by: Fritz Brandhuber <fritz.brandhuber@tngtech.com>

---
+++ ArchUnitNETTests/Loader/NestedTypesTests.cs
@@ -0,0 +1,53 @@
+﻿//  Copyright 2019 Florian Gather <florian.gather@tngtech.com>
+// 	Copyright 2019 Fritz Brandhuber <fritz.brandhuber@tngtech.com>
+// 	Copyright 2020 Pavel Fischer <rubbiroid@gmail.com>
+// 
+// 	SPDX-License-Identifier: Apache-2.0
+// 
+
+using ArchUnitNET.Domain;
+using ArchUnitNET.Loader;
+using ArchUnitNET.xUnit;
+using Xunit;
+using static ArchUnitNET.Fluent.ArchRuleDefinition;
+
+namespace ArchUnitNETTests.Loader
+{
+    public class NestedTypesTests
+    {
+        private static readonly Architecture Architecture =
+            new ArchLoader().LoadAssemblies(typeof(NestedTypesTests).Assembly).Build();
+
+        [Fact]
+        public void FindAllNestedTypes()
+        {
+            Types().That().Are(typeof(Layer1Class.Layer2Class)).Should().Exist().Check(Architecture);
+            Types().That().Are(typeof(Layer1Class.Layer2Class.Layer3Class1)).Should().Exist().Check(Architecture);
+            Types().That().Are(typeof(Layer1Class.Layer2Class.Layer3Class1.Layer4Class1)).Should().Exist()
+                .Check(Architecture);
+            Types().That().Are(typeof(Layer1Class.Layer2Class.Layer3Class2)).Should().Exist().Check(Architecture);
+            Types().That().Are(typeof(Layer1Class.Layer2Class.Layer3Class2.Layer4Class2)).Should().Exist()
+                .Check(Architecture);
+        }
+    }
+
+    public class Layer1Class
+    {
+        public class Layer2Class
+        {
+            public class Layer3Class1
+            {
+                public class Layer4Class1
+                {
+                }
+            }
+
+            public class Layer3Class2
+            {
+                public class Layer4Class2
+                {
+                }
+            }
+        }
+    }
+}
