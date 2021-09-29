3db719751bb462f0bbfcb6088828ad6407dabbc7
Stephan Joneleit <stephan.joneleit@zeiss.com>
2021-09-29 08:44:07 +0200

    Add: Test PUML;
    Signed-off-by: Stephan Joneleit <stephan.joneleit@zeiss.com>

---
+++ ArchUnitNet.MSTest.CheckErrormessages/PlantUMLErrormessagesCheck.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ArchUnitNet.MSTest.CheckErrormessages
+{
+    [TestClass]
+    public class PlantUMLErrormessagesCheck
+    {
+        [TestMethod]
+        public void StartTest()
+        {
+
+        }
+    }
+}
