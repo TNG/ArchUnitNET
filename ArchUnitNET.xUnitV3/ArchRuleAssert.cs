ec66c31534134c403d9fec77bee195c99a367f5e
Jonas Bengtsson <jonas@bengtsson.cc>
2025-02-11 22:54:13 +0100

    Issue: #338
    Add support for xunit.v3
    
    Signed-off-by: Jonas Bengtsson <jonas@bengtsson.cc>

---
+++ ArchUnitNET.xUnitV3/ArchRuleAssert.cs
@@ -0,0 +1,30 @@
+﻿//  Copyright 2019 Florian Gather <florian.gather@tngtech.com>
+// 	Copyright 2019 Paula Ruiz <paularuiz22@gmail.com>
+// 	Copyright 2019 Fritz Brandhuber <fritz.brandhuber@tngtech.com>
+// 	Copyright 2025 Jonas Bengtsson <jonas@bengtsson.cc>
+//
+// 	SPDX-License-Identifier: Apache-2.0
+
+using ArchUnitNET.Domain;
+using ArchUnitNET.Fluent;
+
+// ReSharper disable once CheckNamespace
+namespace ArchUnitNET.xUnitV3
+{
+    public static class ArchRuleAssert
+    {
+        /// <summary>
+        ///     Verifies that the architecture meets the criteria of the archrule.
+        /// </summary>
+        /// <param name="architecture">The architecture to be tested</param>
+        /// <param name="archRule">The rule to test the architecture with</param>
+        /// <exception cref="FailedArchRuleException">Thrown if the rule is violated</exception>
+        public static void CheckRule(Architecture architecture, IArchRule archRule)
+        {
+            if (!archRule.HasNoViolations(architecture))
+            {
+                throw new FailedArchRuleException(architecture, archRule);
+            }
+        }
+    }
+}
---
+++ ArchUnitNET.xUnitV3/FailedArchRuleException.cs
@@ -0,0 +1,34 @@
+﻿//  Copyright 2019 Florian Gather <florian.gather@tngtech.com>
+// 	Copyright 2019 Paula Ruiz <paularuiz22@gmail.com>
+// 	Copyright 2019 Fritz Brandhuber <fritz.brandhuber@tngtech.com>
+// 	Copyright 2025 Jonas Bengtsson <jonas@bengtsson.cc>
+//
+// 	SPDX-License-Identifier: Apache-2.0
+
+using System.Collections.Generic;
+using ArchUnitNET.Domain;
+using ArchUnitNET.Fluent;
+using ArchUnitNET.Fluent.Extensions;
+using Xunit.Sdk;
+
+// ReSharper disable once CheckNamespace
+namespace ArchUnitNET.xUnitV3
+{
+    public class FailedArchRuleException : XunitException
+    {
+        /// <summary>
+        ///     Creates a new instance of the <see href="FailedArchRuleException" /> class.
+        /// </summary>
+        /// <param name="architecture">The architecture which was tested</param>
+        /// <param name="archRule">The archrule that failed</param>
+        public FailedArchRuleException(Architecture architecture, IArchRule archRule)
+            : this(archRule.Evaluate(architecture)) { }
+
+        /// <summary>
+        ///     Creates a new instance of the <see href="FailedArchRuleException" /> class.
+        /// </summary>
+        /// <param name="evaluationResults">The results of the evaluation of the archrule</param>
+        public FailedArchRuleException(IEnumerable<EvaluationResult> evaluationResults)
+            : base(evaluationResults.ToErrorMessage()) { }
+    }
+}
