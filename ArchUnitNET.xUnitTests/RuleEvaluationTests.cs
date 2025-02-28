14b3263044800284c60821c42379b8fb64b7f247
Jonas Bengtsson <jonas@bengtsson.cc>
2025-02-28 09:33:08 +0100

    Fix review comment
    Signed-off-by: Jonas Bengtsson <jonas@bengtsson.cc>

--- ArchUnitNET.xUnitTests/RuleEvaluationTests.cs
+++ ArchUnitNET.xUnitTests/RuleEvaluationTests.cs
@@ -1,10 +1,3 @@
-//  Copyright 2019 Florian Gather <florian.gather@tngtech.com>
-//  Copyright 2019 Paula Ruiz <paularuiz22@gmail.com>
-//  Copyright 2019 Fritz Brandhuber <fritz.brandhuber@tngtech.com>
-//  Copyright 2025 Jonas Bengtsson <jonas@bengtsson.cc>
-//
-//  SPDX-License-Identifier: Apache-2.0
-
 using ArchUnitNET.Domain;
 using ArchUnitNET.Fluent;
 using ArchUnitNET.Fluent.Extensions;
--- ArchUnitNET.xUnitV3/FailedArchRuleException.cs
+++ ArchUnitNET.xUnitV3/FailedArchRuleException.cs
@@ -1,11 +1,4 @@
-﻿//  Copyright 2019 Florian Gather <florian.gather@tngtech.com>
-// 	Copyright 2019 Paula Ruiz <paularuiz22@gmail.com>
-// 	Copyright 2019 Fritz Brandhuber <fritz.brandhuber@tngtech.com>
-// 	Copyright 2025 Jonas Bengtsson <jonas@bengtsson.cc>
-//
-// 	SPDX-License-Identifier: Apache-2.0
-
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using ArchUnitNET.Domain;
 using ArchUnitNET.Fluent;
 using ArchUnitNET.Fluent.Extensions;
