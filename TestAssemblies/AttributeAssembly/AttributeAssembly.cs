87dff1cb21f5ed0b529544f1f0a58311df5f882e
Alexander Linne <alexander.linne@tngtech.com>
2025-07-11 11:48:24 +0200

    test: refactor AttributeAssembly

--- TestAssemblies/AttributeAssembly/AttributeAssembly.cs
+++ TestAssemblies/AttributeAssembly/AttributeAssembly.cs
@@ -2,16 +2,6 @@
 
 namespace AttributeNamespace;
 
-public class TypeArgument { };
-
-public class OtherTypeArgument { };
-
-public class NamedTypeArgument { }
-
-public class OtherNamedTypeArgument { }
-
-public class UnusedTypeArgument { }
-
 [System.AttributeUsage(System.AttributeTargets.Class | System.AttributeTargets.Struct)]
 public class Attribute1 : System.Attribute
 {
@@ -31,11 +21,24 @@ public class Attribute2 : System.Attribute
     public Attribute2(System.Type? parameter1 = null, string parameter2 = "", int parameter3 = -1)
     { }
 
-    public string OtherNamedParameter1 { get; set; } = "";
+    public string NamedParameter1 { get; set; } = "";
 
-    public int OtherNamedParameter2 { get; set; }
+    public int NamedParameter2 { get; set; }
 
-    public System.Type? OtherNamedParameter3 { get; set; }
+    public System.Type? NamedParameter3 { get; set; }
+}
+
+[System.AttributeUsage(System.AttributeTargets.Class | System.AttributeTargets.Struct)]
+public class Attribute3 : System.Attribute
+{
+    public Attribute3(int parameter3 = -1, System.Type? parameter1 = null, string parameter2 = "")
+    { }
+
+    public int NamedParameter1 { get; set; }
+
+    public System.Type? NamedParameter2 { get; set; }
+
+    public string NamedParameter3 { get; set; } = "";
 }
 
 [System.AttributeUsage(System.AttributeTargets.Class | System.AttributeTargets.Struct)]
@@ -56,54 +59,89 @@ public class UnusedAttribute : System.Attribute
 
 public class ClassWithoutAttributes { }
 
-public class OtherClassWithoutAttributes { }
-
 [Attribute1]
 public class ClassWithSingleAttribute { }
 
 [Attribute1]
-public class OtherClassWithSingleAttribute { }
+[Attribute2]
+public class ClassWithTwoAttributes { }
 
 [Attribute1]
 [Attribute2]
-public class ClassWithAttributes { }
+[Attribute3]
+public class ClassWithThreeAttributes { }
 
-[Attribute1]
-[Attribute2]
-public class OtherClassWithAttributes { }
+public class TypeArgument1 { };
+
+public class TypeArgument2 { };
+
+public class TypeArgument3 { };
+
+public class UnusedTypeArgument { };
+
+[Attribute1("Argument1", 1, typeof(TypeArgument1))]
+public class ClassWithSingleAttributeWithArguments { }
+
+[Attribute1("Argument1", 1, typeof(TypeArgument1))]
+[Attribute2(typeof(TypeArgument2), "Argument2", 2)]
+public class ClassWithTwoAttributesWithArguments { }
+
+[Attribute1("Argument1", 1, typeof(TypeArgument1))]
+[Attribute2(typeof(TypeArgument2), "Argument2", 2)]
+[Attribute3(3, typeof(TypeArgument3), "Argument3")]
+public class ClassWithThreeAttributesWithArguments { }
 
 [Attribute1(
-    "Argument",
-    0,
-    typeof(TypeArgument),
-    NamedParameter1 = typeof(NamedTypeArgument),
-    NamedParameter2 = "NamedArgument",
+    NamedParameter1 = typeof(TypeArgument1),
+    NamedParameter2 = "Argument1",
+    NamedParameter3 = 1
+)]
+public class ClassWithSingleAttributeWithNamedArguments { }
+
+[Attribute1(
+    NamedParameter1 = typeof(TypeArgument1),
+    NamedParameter2 = "Argument1",
     NamedParameter3 = 1
 )]
 [Attribute2(
-    typeof(OtherTypeArgument),
-    "OtherArgument",
-    2,
-    OtherNamedParameter1 = "OtherNamedArgument",
-    OtherNamedParameter2 = 3,
-    OtherNamedParameter3 = typeof(OtherNamedTypeArgument)
+    NamedParameter1 = "Argument2",
+    NamedParameter2 = 2,
+    NamedParameter3 = typeof(TypeArgument2)
 )]
-public class ClassWithArguments { }
+public class ClassWithTwoAttributesWithNamedArguments { }
 
 [Attribute1(
-    "Argument",
-    0,
-    typeof(TypeArgument),
-    NamedParameter1 = typeof(NamedTypeArgument),
-    NamedParameter2 = "NamedArgument",
+    NamedParameter1 = typeof(TypeArgument1),
+    NamedParameter2 = "Argument1",
     NamedParameter3 = 1
 )]
 [Attribute2(
-    typeof(OtherTypeArgument),
-    "OtherArgument",
-    2,
-    OtherNamedParameter1 = "OtherNamedArgument",
-    OtherNamedParameter2 = 3,
-    OtherNamedParameter3 = typeof(OtherNamedTypeArgument)
+    NamedParameter1 = "Argument2",
+    NamedParameter2 = 2,
+    NamedParameter3 = typeof(TypeArgument2)
 )]
-public class OtherClassWithArguments { }
+[Attribute3(
+    NamedParameter1 = 3,
+    NamedParameter2 = typeof(TypeArgument3),
+    NamedParameter3 = "Argument3"
+)]
+public class ClassWithThreeAttributesWithNamedArguments { }
+
+[System.AttributeUsage(System.AttributeTargets.Class | System.AttributeTargets.Struct)]
+public class OnceUsedAttribute : System.Attribute
+{
+    public OnceUsedAttribute(
+        string parameter1 = "",
+        int parameter2 = -1,
+        System.Type? parameter3 = null
+    ) { }
+
+    public System.Type? NamedParameter1 { get; set; }
+
+    public string NamedParameter2 { get; set; } = "";
+
+    public int NamedParameter3 { get; set; }
+}
+
+[OnceUsed]
+public class ClassWithSingleUniquelyUsedAttribute { }
