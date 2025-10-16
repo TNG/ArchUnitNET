84f4bc5fcde7c88209b23cb0bfc06b5e9fd0e8e7
SeMuell <s.mueller@gmx.org>
2025-10-16 18:32:45 +0200

    Add MSTestV4 Support and add Proper Test Projects
    Signed-off-by: SeMuell <s.mueller@gmx.org>

---
+++ ArchUnitNET.MSTestV4/ArchRuleAssert.cs
@@ -0,0 +1,23 @@
+﻿using ArchUnitNET.Domain;
+using ArchUnitNET.Fluent;
+using ArchUnitNET.Fluent.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ArchUnitNET.MSTestV4
+{
+    public static class ArchRuleAssert
+    {
+        /// <summary>
+        ///     Verifies that the architecture meets the criteria of the archrule.
+        /// </summary>
+        /// <param name="architecture">The architecture to be tested</param>
+        /// <param name="archRule">The rule to test the architecture with</param>
+        public static void FulfilsRule(Architecture architecture, IArchRule archRule)
+        {
+            if (!archRule.HasNoViolations(architecture))
+            {
+                Assert.Fail(archRule.Evaluate(architecture).ToErrorMessage());
+            }
+        }
+    }
+}
