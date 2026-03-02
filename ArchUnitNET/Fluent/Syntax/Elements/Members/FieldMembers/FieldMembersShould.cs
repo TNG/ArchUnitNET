955e8b56548834a99d10b0c98a2c29b561ba41e6
Alexander Linne <alexander.linne@tngtech.com>
2026-03-02 18:08:49 +0100

    fix: FieldMembersShould should yield a FieldMembersShouldConjunction
    Signed-off-by: Alexander Linne <alexander.linne@tngtech.com>

--- ArchUnitNET/Fluent/Syntax/Elements/Members/FieldMembers/FieldMembersShould.cs
+++ ArchUnitNET/Fluent/Syntax/Elements/Members/FieldMembers/FieldMembersShould.cs
@@ -3,17 +3,17 @@ using ArchUnitNET.Fluent.Conditions;
 
 namespace ArchUnitNET.Fluent.Syntax.Elements.Members.FieldMembers
 {
-    public sealed class FieldMembersShould : AddFieldMemberCondition<FieldMembersShould>
+    public sealed class FieldMembersShould : AddFieldMemberCondition<FieldMembersShouldConjunction>
     {
         public FieldMembersShould(IArchRuleCreator<FieldMember> ruleCreator)
             : base(ruleCreator) { }
 
-        protected override FieldMembersShould CreateNextElement(
+        protected override FieldMembersShouldConjunction CreateNextElement(
             IOrderedCondition<FieldMember> condition
         )
         {
             _ruleCreator.AddCondition(condition);
-            return new FieldMembersShould(_ruleCreator);
+            return new FieldMembersShouldConjunction(_ruleCreator);
         }
     }
 }
