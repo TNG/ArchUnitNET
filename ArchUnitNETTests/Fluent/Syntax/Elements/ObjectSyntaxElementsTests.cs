0300f12e0618f13aef58270babb295dbe47dca02
Alexander Linne <alexander.linne@tngtech.com>
2026-08-28 08:11:35 +0200

    test: cover GivenObjects catch blocks and FormatDescription
    A bare GivenObjects (not a "...That()" conjunction) passed as an object
    provider routes through GivenObjects.FormatDescription instead of a
    "...WithDescription" conjunction's custom description. And a provider
    backed by a CombinedArchRuleCreator can't produce a filtered object list,
    so GetObjects() turns the internal exception into the user-facing
    CannotGetObjectsOfCombinedArchRuleException. Both paths were reachable
    through the public fluent API but untested.
    
    Split into two tests, BareObjectProviderDescriptionTest and
    GetObjectsOnCombinedArchRuleTest, since they cover unrelated code paths
    that only happened to share a name.
    
    Signed-off-by: Alexander Linne <alexander.linne@tngtech.com>

--- ArchUnitNETTests/Fluent/Syntax/Elements/ObjectSyntaxElementsTests.cs
+++ ArchUnitNETTests/Fluent/Syntax/Elements/ObjectSyntaxElementsTests.cs
@@ -6,6 +6,7 @@ using ArchUnitNET.Domain;
 using ArchUnitNET.Domain.Exceptions;
 using ArchUnitNET.Fluent;
 using ArchUnitNET.Fluent.Conditions;
+using ArchUnitNET.Fluent.Exceptions;
 using ArchUnitNET.Fluent.Predicates;
 using ArchUnitNET.Fluent.Syntax.Elements.Types;
 using ArchUnitNETTests.AssemblyTestHelper;
@@ -449,6 +450,42 @@ public class ObjectSyntaxElementsTests
 
         helper.AddSnapshotHeader("Violations");
         Types().That().HaveFullName(helper.NonExistentObjectName).Should().Exist().AssertOnlyViolations(helper);
+        await helper.AssertSnapshotMatches();
+    }
+
+    [Fact]
+    public async Task BareObjectProviderDescriptionTest()
+    {
+        var helper = new TypeDependencyAssemblyTestHelper();
+
+        // A bare GivenObjects (not a "...That()" conjunction) passed as an object provider argument
+        // routes its description through GivenObjects.FormatDescription instead of a "...WithDescription"
+        // conjunction's custom description.
+        helper.AddSnapshotHeader("Bare object provider");
+        Types().That().Are(helper.ChildClass).Should().BeAssignableTo(Types()).AssertNoViolations(helper);
+
+        await helper.AssertSnapshotMatches();
+    }
+
+    [Fact]
+    public async Task GetObjectsOnCombinedArchRuleTest()
+    {
+        var helper = new TypeDependencyAssemblyTestHelper();
+
+        helper.AddSnapshotHeader("Combined arch rule as object provider");
+        var someRule = Types().That().Are(helper.ChildClass).Should().Exist();
+        var combinedProvider = someRule.And().Types();
+        Types()
+            .Should()
+            .BeAssignableTo(combinedProvider)
+            .AssertException<CannotGetObjectsOfCombinedArchRuleException>(helper);
+
+        var describedCombinedProvider = someRule.And().Types().That().AreValueTypes().As("combined types");
+        Types()
+            .Should()
+            .BeAssignableTo(describedCombinedProvider)
+            .AssertException<CannotGetObjectsOfCombinedArchRuleException>(helper);
+
         await helper.AssertSnapshotMatches();
     }
 
